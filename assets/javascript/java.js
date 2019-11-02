803f537560cdb68fc40d9d697da76cbdbff88976
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-02 09:34:21 -0400

    added blank word div

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -2,12 +2,14 @@ var num_of_guesses = 5;
 var list_of_words = ["stop","affirm","conquest"]
 var computer_Guess = list_of_words[Math.floor(Math.random() * list_of_words.length)];
 var results = [];
+var blankWord = []; 
 console.log (computer_Guess);
 for (var i = 0; i < computer_Guess.length; i++) {
     results[i] = " _ ";
     
 }
-document.onkeyup = function(event) {
+    
+    document.onkeyup = function(event) {
     // Determines which key was pressed.
     var user_Guess = event.key;
     
@@ -15,6 +17,7 @@ document.onkeyup = function(event) {
         if (user_Guess === computer_Guess.charAt(j)) {
             results[j] = user_Guess;
             console.log (results);
+            $("#blankWord").text(results);
         }
     }
 
