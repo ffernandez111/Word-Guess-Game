ed44886f6bcd3778fdc7089303b23ff32e408a35
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-02 10:01:19 -0400

    created else statement for wrong guess

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,6 +1,6 @@
-var num_of_guesses = 5;
 var list_of_words = ["stop","affirm","conquest"]
 var computer_Guess = list_of_words[Math.floor(Math.random() * list_of_words.length)];
+var num_of_guesses = computer_Guess.length + 5;
 var results = [];
 var blankWord = []; 
 console.log (computer_Guess);
@@ -14,10 +14,14 @@ for (var i = 0; i < computer_Guess.length; i++) {
     var user_Guess = event.key;
     
     for (var j = 0; j < computer_Guess.length; j++) {
+        // Hit
         if (user_Guess === computer_Guess.charAt(j)) {
             results[j] = user_Guess;
             console.log (results);
             $("#blankWord").text(results);
+        } else {
+            // Miss
+            console.log('Wrong letter:', user_Guess);
         }
     }
 
